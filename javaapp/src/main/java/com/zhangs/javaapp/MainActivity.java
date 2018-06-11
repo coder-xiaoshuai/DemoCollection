431d2947cd16c60c8ec9d553ec36c9972b4a1e6c
zhangshuai <540958710@qq.com>
2018-06-11 18:13:32 +0800

    kotlin学习

---
+++ javaapp/src/main/java/com/zhangs/javaapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.zhangs.javaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private WebView mWebView;
+    private Button mButton;
+    private String data="<p>大概3个月前吧。</p><p>无意之间看到了好久不联系的初恋动态：澳洲留学  高 帅（富不富我不知道/不过看样子日子过得很不错）还有一个很可爱的女票。我在几分钟内刷完了他的动态，基本上就是他和女票的恋爱日常，非常幸福非常甜。再看看自己，一个死肥宅。我承认我有点难过，当时我愣了整整一节课。下课后就在心里告诉自己：我得做出一些改变。然后晚上就去学校附近的健身房办卡。3个月了吧，每周固定去健身房4.5次，雷打不动。刚开始的时候，真的很累，好多次单车冲刺的时候我都累到哭，但一想到初恋的脸，腿上又来劲了。距3个月前已经瘦了16斤了，生活越来越美好hhhhha~</p><p>我值得更好的。加油  还要继续瘦下去hhh(*ˉ︶ˉ*)</p><div><div>明亡于1644就是个混蛋的说法，是对汉族的侮辱，对历史的篡改，明亡于北京城破，那南宋为何不亡于临安城破而是亡于崖山？或者开封城破正统就改成大金，南宋就不能算正经朝代？1644年明亡，那么此后二十年为了抗清而牺牲的几十万军民都成了贼寇叛逆？南明诸帝，有土有民有社稷，凭什么给一笔勾销掉？</div><br></div><h2 style=\"margin-top: 1.66667em; margin-bottom: 0.83333em; font-variant-numeric: inherit; font-variant-east-asian: inherit; font-weight: 600; font-stretch: inherit; font-size: 1.2em; font-family: -apple-system, system-ui, \" helvetica=\"\" neue\",=\"\" \"pingfang=\"\" sc\",=\"\" \"microsoft=\"\" yahei\",=\"\" \"source=\"\" han=\"\" sans=\"\" \"noto=\"\" cjk=\"\" \"wenquanyi=\"\" micro=\"\" hei\",=\"\" sans-serif;=\"\" color:=\"\" rgb(26,=\"\" 26,=\"\" 26);=\"\" background-color:=\"\" rgb(255,=\"\" 255,=\"\" 255);\"=\"\">何谓“大体重”</h2><figure style=\"margin-top: 1em; margin-bottom: 1em; color: rgb(26, 26, 26); font-family: -apple-system, system-ui, \" helvetica=\"\" neue\",=\"\" \"pingfang=\"\" sc\",=\"\" \"microsoft=\"\" yahei\",=\"\" \"source=\"\" han=\"\" sans=\"\" \"noto=\"\" cjk=\"\" \"wenquanyi=\"\" micro=\"\" hei\",=\"\" sans-serif;=\"\" font-size:=\"\" 15px;=\"\" background-color:=\"\" rgb(255,=\"\" 255,=\"\" 255);\"=\"\"><img src=\"http://pcimage.collegewiki.com.cn/webpc/upload/article/7aaa5a8232de5b5114bde2c91e90e592.jpeg?imageMogr2/format/jpg\" normal\"=\"\" width=\"960\" style=\"display: block; max-width: 100%; margin-right: auto; margin-left: auto; cursor: zoom-in;\" class=\"\"></figure><p style=\"color: rgb(26, 26, 26); font-size: 15px; background-color: rgb(255, 255, 255);\"><br></p><p style=\"color: rgb(26, 26, 26); font-size: 15px; background-color: rgb(255, 255, 255);\">通俗来说，也就是肥胖。</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\"><img _width=\"100%\" src=\"http://pcimage.collegewiki.com.cn/webpc/upload/article/39f651b748246a00d090cb6c2593bb92.webp?imageMogr2/format/jpg\" style=\"max-width: 100%; width: 636px; height: auto !important; word-wrap: break-word !important; visibility: visible !important;\" class=\"\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">每年的毕业季，也是很多毕业生一年一度制定计划的时候。</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\"><span style=\"max-width: 100%; color: rgb(255, 169, 0); word-wrap: break-word !important;\"><strong style=\"max-width: 100%; word-wrap: break-word !important;\">Pauli Murray</strong></span>在1945年写过一篇关于她抱负的文章，当时的她，是一名年轻的律师和民权活动家。</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\"><img _width=\"100%\" src=\"http://pcimage.collegewiki.com.cn/webpc/upload/article/af29736c9cc7141e5f2bb89bf1f20af5.webp?imageMogr2/format/jpg\" style=\"max-width: 100%; width: 636px; height: auto !important; word-wrap: break-word !important; visibility: visible !important;\" class=\"\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; background-color: rgb(255, 255, 255); text-align: center; line-height: 1.75em; word-wrap: break-word !important;\"><span style=\"max-width: 100%; font-size: 14px; color: rgb(136, 136, 136); word-wrap: break-word !important;\">（图源：wikipedia）</span></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">“我打算用积极和包容的方法来摧毁种族隔离，”Murray在当时写道。</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\"><span style=\"max-width: 100%; color: rgb(255, 169, 0); word-wrap: break-word !important;\"><strong style=\"max-width: 100%; word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></strong></span></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\"><span style=\"max-width: 100%; color: rgb(255, 169, 0); word-wrap: break-word !important;\"><strong style=\"max-width: 100%; word-wrap: break-word !important;\">“当我的族类们试着画一个圈来排斥我时，我将画一个更大的圆来包含它们。他们为弱势群体的特权大声疾呼，而我将为全人类的权利呐喊。”</strong></span></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">那么问题来了：</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">如果是你，你会画一个什么样的圈呢？</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">是一个大的、包容的、充满活力的圆圈，还是一个会缩小、变得“微不足道”、享有特权的圈？</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">包容的工作虽然没那么容易，但回报却是巨大的。</p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><img _width=\"100%\" src=\"http://pcimage.collegewiki.com.cn/webpc/upload/article/aae34bf889ad5d8c8a1bfabcea86817f.webp?imageMogr2/format/jpg\" style=\"max-width: 100%; width: 636px; height: auto !important; word-wrap: break-word !important; visibility: visible !important;\" class=\"\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); word-wrap: break-word !important;\"><br style=\"max-width: 100%; word-wrap: break-word !important;\"></p><p style=\"max-width: 100%; clear: both; min-height: 1em; color: rgb(51, 51, 51); font-size: 17px; letter-spacing: 0.544px; text-align: justify; background-color: rgb(255, 255, 255); line-height: 1.75em; word-wrap: break-word !important;\">当你们离开校园时，让我建议你们可以参考Pauli Murray和许多其他耶鲁大学毕业生的例子。</p>";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView=findViewById(R.id.main_webview);
+        mButton=findViewById(R.id.main_btn_clearcache);
+
+        mWebView.setWebChromeClient(new WebChromeClient());
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        /* 设置为true时表示支持使用js打开新的窗口 */
+        mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        /* 当网页需要保存数时据,设置下面属性 */
+        mWebView.getSettings().setDomStorageEnabled(true);
+        /* 设置为使用webview推荐的窗口，主要是为了配合下一个属性 */
+        mWebView.getSettings().setUseWideViewPort(true);
+        /* 设置网页自适应屏幕大小，该属性必须和上一属性配合使用 */
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        /* 启用还H5的地理定位服务 */
+        mWebView.getSettings().setGeolocationEnabled(true);
+        /* 设置是否允许webview使用缩放的功能 */
+        mWebView.getSettings().setBuiltInZoomControls(false);
+        /* 设置是否显示水平滚动条 */
+        mWebView.setHorizontalScrollBarEnabled(false);
+        /* 设置垂直滚动条是否有叠加样式 */
+        mWebView.setVerticalScrollbarOverlay(true);
+        /* 设置滚动条的样式 */
+        mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        /*设置缓存策略*/
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
+
+        mWebView.loadDataWithBaseURL("about:blank",data,null,null,null);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebView.clearCache(true);
+                mWebView.clearFormData();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
